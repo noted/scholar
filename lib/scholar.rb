60b4ecd0ff03e58d0cbd24520c99390119dda589
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-03-06 22:17:04 -0500

    oops, forgot to remove lib/scholar/client.rb from lib/scholar.rb.

--- lib/scholar.rb
+++ lib/scholar.rb
@@ -1,7 +1,6 @@
 require 'json'
 require 'active_support/all'
 
-require_relative 'scholar/client'
 require_relative 'scholar/utilities'
 require_relative 'scholar/citation'
 require_relative 'scholar/source'
