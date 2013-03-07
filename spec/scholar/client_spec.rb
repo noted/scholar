9c2b6fe9198d4bd4eb818bbd52fe0db7851be9e3
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-03-06 22:16:01 -0500

    removed Client (for the moment).

--- spec/scholar/client_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Scholar::Client do
-  describe ".initialize" do
-    let(:client) { Scholar::Client.new }
-
-    it { client.should be_an_instance_of Scholar::Client }
-  end
-end
