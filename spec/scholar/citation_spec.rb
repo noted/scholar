8840d3ae8aa8f1382d71bafa02590472defaa08f
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-02-09 12:45:36 -0500

    added some simple specs for ::Citation.

---
+++ spec/scholar/citation_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Scholar::Citation do
+  let(:citation) do
+    client.cite({
+      "author" => [
+        {
+          "given" => "Edgar Allen",
+          "family"=>"Poe"
+        }
+      ],
+      "title" => "Poetry, Tales, and Selected Essays",
+      "type" => "book",
+      "issued" => {
+        "date-parts"=> [ [ 1996 ] ]
+      },
+      "editor"=> [
+        {
+          "family" => "Quinn",
+          "given" => "Patrick F."
+        },
+        {
+          "family" => "Thompson",
+          "given" => "G.R."
+        }
+      ],
+      "publisher" => "Library of America",
+      "publisher-place" => "New York"
+    })
+  end
+
+  describe '.new' do
+    it 'returns a correct instance' do
+      expect(citation).to be_an_instance_of Scholar::Citation
+    end
+  end
+end
